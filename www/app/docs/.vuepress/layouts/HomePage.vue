bb9372f7b24a65afa0afad78b23d6c2a65d86497
aanthonymax <anthonymaxgit@gmail.com>
2025-06-02 10:05:15 +0300

    add producthunt badge

--- www/app/docs/.vuepress/layouts/HomePage.vue
+++ www/app/docs/.vuepress/layouts/HomePage.vue
@@ -795,33 +795,18 @@
         </div>
       </div>
     </div>
-    <!-- <a
-      href="https://github.com/hmpl-language/hmpl"
-      target="_blank"
-      class="star_us"
-    >
-      <div class="star_us_content">
-        <div class="star_us_block star_us_block_1">
-          <span class="star_us_text star_us_text_1">Star Us</span>
-          <span class="star_us_star">★</span>
-        </div>
-        <div class="star_us_block star_us_block_2 hidden">
-          <span class="star_us_text star_us_text_2">On GitHub</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="star_us_github"
-            viewBox="0 0 1024 1024"
-            aria-label="github icon"
-            fill="#09090b"
-            name="github"
-          >
-            <path
-              d="M511.957 21.333C241.024 21.333 21.333 240.981 21.333 512c0 216.832 140.544 400.725 335.574 465.664 24.49 4.395 32.256-10.07 32.256-23.083 0-11.69.256-44.245 0-85.205-136.448 29.61-164.736-64.64-164.736-64.64-22.315-56.704-54.4-71.765-54.4-71.765-44.587-30.464 3.285-29.824 3.285-29.824 49.195 3.413 75.179 50.517 75.179 50.517 43.776 75.008 114.816 53.333 142.762 40.79 4.523-31.66 17.152-53.377 31.19-65.537-108.971-12.458-223.488-54.485-223.488-242.602 0-53.547 19.114-97.323 50.517-131.67-5.035-12.33-21.93-62.293 4.779-129.834 0 0 41.258-13.184 134.912 50.346a469.803 469.803 0 0 1 122.88-16.554c41.642.213 83.626 5.632 122.88 16.554 93.653-63.488 134.784-50.346 134.784-50.346 26.752 67.541 9.898 117.504 4.864 129.834 31.402 34.347 50.474 78.123 50.474 131.67 0 188.586-114.73 230.016-224.042 242.09 17.578 15.232 33.578 44.672 33.578 90.454v135.85c0 13.142 7.936 27.606 32.854 22.87C862.25 912.597 1002.667 728.747 1002.667 512c0-271.019-219.648-490.667-490.71-490.667z"
-            ></path>
-          </svg>
-        </div>
-      </div>
-    </a> -->
+    <div class="producthunt">
+      <a
+        href="https://www.producthunt.com/products/hmpl-js?embed=true&utm_source=badge-featured&utm_medium=badge&utm_source=badge-hmpl&#0045;js"
+        target="_blank"
+        ><img
+          src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=972730&theme=dark&t=1748847658573"
+          alt="HMPL&#0046;js - Template&#0032;language&#0032;for&#0032;displaying&#0032;UI&#0032;from&#0032;server&#0032;to&#0032;client | Product Hunt"
+          style="width: 250px; height: 54px"
+          width="250"
+          height="54"
+      /></a>
+    </div>
   </footer>
 </template>
 <script>
@@ -1023,20 +1008,11 @@ a {
   color: #fff;
 }
 
-.star_us {
+.producthunt {
   position: fixed;
   left: 10px;
   bottom: 4rem;
   inset-inline-start: 1rem;
-  width: 140px;
-  height: 48px;
-  border-radius: 30px;
-  background-color: #fff;
-  border: #e4e4e7 1px solid;
-  box-shadow:
-    0 0 #0000,
-    0 0 #0000,
-    0 1px 2px 0 rgba(0, 0, 0, 0.05);
   z-index: 100;
   display: flex;
   align-items: center;
@@ -1046,8 +1022,8 @@ a {
 }
 
 @media (max-width: 600px) {
-  .star_us {
-    bottom: 2rem;
+  .producthunt {
+    bottom: 1.8rem;
   }
 }
 
